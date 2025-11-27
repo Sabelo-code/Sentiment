01e1ae9a7915cbec9c51111ed69f6bd9317c190e
Sabelo-code <114867440+Sabelo-code@users.noreply.github.com>
2025-11-27 11:47:16 +0200

    Fix firebaseConfig syntax for Python dictionary

--- firebase_config.py
+++ firebase_config.py
@@ -1,14 +1,14 @@
 import pyrebase
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA4AXSkS7Dhs134E2Kwc0UIW9FaDfSdCVE",
-  authDomain: "sentiment-analysis-f6136.firebaseapp.com",
-  projectId: "sentiment-analysis-f6136",
-  storageBucket: "sentiment-analysis-f6136.firebasestorage.app",
-  messagingSenderId: "455254508762",
-  appId: "1:455254508762:web:18c3105e1fd244834b91e3",
-  measurementId: "G-VM7KHC7WGV"
-};
+firebaseConfig = {
+    "apiKey": "AIzaSyA4AXSkS7Dhs134E2Kwc0UIW9FaDfSdCVE",
+  "authDomain": "sentiment-analysis-f6136.firebaseapp.com",
+  "projectId": "sentiment-analysis-f6136",
+  "storageBucket": "sentiment-analysis-f6136.firebasestorage.app",
+  "messagingSenderId": "455254508762",
+  "appId": "1:455254508762:web:18c3105e1fd244834b91e3",
+  "measurementId": "G-VM7KHC7WGV"
+}
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
