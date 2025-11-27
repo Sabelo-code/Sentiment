c3d3ab5aa344455b87c12ddcc6f1701b9ef088f8
Sabelo-code <114867440+Sabelo-code@users.noreply.github.com>
2025-11-27 12:39:47 +0200

    Refactor firebase_config.py for database access
    Updated firebase_config.py to include database initialization and corrected formatting.

--- firebase_config.py
+++ firebase_config.py
@@ -1,15 +1,20 @@
-import pyrebase
+with open("firebase_config.py", "w") as f:
+    f.write("""import pyrebase
 
 firebaseConfig = {
     "apiKey": "AIzaSyA4AXSkS7Dhs134E2Kwc0UIW9FaDfSdCVE",
-  "authDomain": "sentiment-analysis-f6136.firebaseapp.com",
-  "projectId": "sentiment-analysis-f6136",
-  "storageBucket": "sentiment-analysis-f6136.firebasestorage.app",
-  "messagingSenderId": "455254508762",
-  "appId": "1:455254508762:web:18c3105e1fd244834b91e3",
-  "measurementId": "G-VM7KHC7WGV"
- "databaseURL" : "https://sentiment-analysis-f6136-default-rtdb.firebaseio.com/"
+    "authDomain": "sentiment-analysis-f6136.firebaseapp.com",
+    "databaseURL": "https://sentiment-analysis-f6136-default-rtdb.firebaseio.com",
+    "projectId": "sentiment-analysis-f6136",
+    "storageBucket": "sentiment-analysis-f6136.firebasestorage.app",
+    "messagingSenderId": "455254508762",
+    "appId": "1:455254508762:web:18c3105e1fd244834b91e3",
+    "measurementId": "G-VM7KHC7WGV"
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
-auth = firebase.auth()
+
+auth = firebase.auth()        # Authentication object
+db = firebase.database()      # Database object
+""")
+
